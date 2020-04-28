e76e42777b0d3044f5d5f52181e324b5b63b767c
Luz Perdomo <lilyperdomo92@gmail.com>
2020-04-28 15:22:36 -0500

    added 4-print_hexa

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for numbers in range (0, 99):
-    print("{:d} {:#02x}".format(numbers, numbers))
+for numbers in range(0, 99):
+    print("{:d} = {:#02x}".format(numbers, numbers))
