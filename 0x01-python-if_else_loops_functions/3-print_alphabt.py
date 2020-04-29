d649b48f46b7d9376d61aa77be077d221c46f558
Luz Perdomo <lilyperdomo92@gmail.com>
2020-04-28 21:05:04 -0500

    fixed style issues

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for letters in range(97, 123):
     if(chr(letters) != 'e' and chr(letters) != 'q'):
-        print("{:c}".format(letters), end = "")
+        print("{:c}".format(letters), end="")
