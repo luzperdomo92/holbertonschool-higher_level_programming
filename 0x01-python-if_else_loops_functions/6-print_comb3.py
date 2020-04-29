fc798ef4d85698d1fd34866d70efaa7e470ffa20
Luz Perdomo <lilyperdomo92@gmail.com>
2020-04-28 20:12:52 -0500

    fixed print combinations

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for comb1 in range(10):
     for comb2 in range(comb1 + 1, 10):
         if comb1 == 8 and comb2 == 9:
-            print("{}{}".format(comb1, comb2))
+            print("{}{}".format(comb1, comb2), end="")
         else:
             print("{}{}, ".format(comb1, comb2), end="")
