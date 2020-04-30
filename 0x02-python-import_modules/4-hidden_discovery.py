70026d07126b1d35307fd223aa305ef4514cc365
Luz Perdomo <lilyperdomo92@gmail.com>
2020-04-30 16:51:47 -0500

    fixed print hidden names

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,4 +3,4 @@ if __name__ == "__main__":
     from hidden_4 import *
     for files in dir():
         if files[:2] != "__":
-            print("{}".format(files)
+            print("{}".format(files))
