d51f3909d089d4bc8449f8fa166b18c9558d6432
Luz Perdomo <lilyperdomo92@gmail.com>
2020-04-30 17:20:47 -0500

    added print arguments inside the liat

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+args = len(sys.argv)
+if args == 1:
+    print("0 arguments.")
+elif args == 2:
+    print("{} argument:".format(args - 1))
+else:
+    print("{} arguments:".format(args - 1))
+    for argm in range(1, args)
+        print("{} : {}".format(argm, sys.args[armg]))
