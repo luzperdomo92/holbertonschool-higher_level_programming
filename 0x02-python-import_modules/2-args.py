c7f6a513983baae10e9d097b17423e74154d8ee4
Luz Perdomo <lilyperdomo92@gmail.com>
2020-04-30 20:55:43 -0500

    fixed again

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,6 +8,7 @@ if args == 1:
     print("0 arguments.")
 elif args == 2:
     print("{} argument:".format(args - 1))
+    print("{}: {}".format(1, sys.argv[1]))
 else:
     print("{} arguments:".format(args - 1))
     for argm in range(1, args)
