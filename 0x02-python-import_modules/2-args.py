9e6852dfbecec767c0d1bc3521db6c2b8bcdfb7d
Luz Perdomo <lilyperdomo92@gmail.com>
2020-04-30 20:49:30 -0500

    fixed

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
+    import sys 
 
 args = len(sys.argv)
+
 if args == 1:
     print("0 arguments.")
 elif args == 2:
@@ -10,4 +11,4 @@ elif args == 2:
 else:
     print("{} arguments:".format(args - 1))
     for argm in range(1, args)
-        print("{} : {}".format(argm, sys.args[armg]))
+        print("{} : {}".format(argm, sys.args[argm]))
