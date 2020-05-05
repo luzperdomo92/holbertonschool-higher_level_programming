9936026cf51e800307d0121b0f247e8e0ddc9b36
Luz Perdomo <lilyperdomo92@gmail.com>
2020-05-05 16:01:17 -0500

    fixed syntax

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python3
 def no_c(my_string):
     if my_string:
-        new_str = my_string
-        new_str = new_str.translate({ord(letter): None for letter in "Cc"})
-        return new_str
-    new_str = my_string
-    return new_str
+        my_string = my_string.translate({ord(letter): None for letter in "Cc"})
+    return my_string
