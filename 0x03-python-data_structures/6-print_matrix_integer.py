fb8f10fa5304340f177ade023d9cd7a58dc83ead
Luz Perdomo <lilyperdomo92@gmail.com>
2020-05-05 17:21:26 -0500

    update second short syntax

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for elem in row:
-            print("{:d}".format(elem), end=' ' if elem == row[-1] else " ")
-        print()
+        print(" ".join("{:d}".format(elem) for elem in row))
