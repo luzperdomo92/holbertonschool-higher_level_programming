533ddb1c2bd26d060d87a3c826b549cca1227a4c
Luz Perdomo <lilyperdomo92@gmail.com>
2020-05-05 17:02:46 -0500

    fixed logical syntax

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for elem in row:
-            print("{:d}".format(elem), end=' ')
-        print()
+    if matrix:
+        for row in matrix:
+            for elem in row:
+                if elem == row[-1]:
+                    print("{:d}".format(elem), end=' ')
+                else:
+                    print("{:d}".format(elem), end=' ')
+            print()
