6f19b4cf34f959a146ad20b4fcead0cd8e83cd12
Luz Perdomo <lilyperdomo92@gmail.com>
2020-05-07 16:06:57 -0500

    fixed old logical

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    return([x if x is not search else replace for x in my_list])
+    new_list = my_list.copy()	
+    for x in range(len(my_list)):	
+        if my_list[x] == search:	
+            new_list[x] = replace	
+    return new_list
