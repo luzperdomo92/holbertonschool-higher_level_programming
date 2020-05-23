aa93ac7f3ea929fc8713dc5b552eadc1d2219f25
Luz Perdomo <lilyperdomo92@gmail.com>
2020-05-23 02:32:00 -0500

    changed comments

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
 """
-    add_integer: Check input if correct, Float arguments are
-    typecasted to ints before addition is performed.
-    Return the integer addition of a and b by default return 98.
+    add_integer module
 """
 
 
 def add_integer(a, b=98):
     """
+    add_integer: Check input if correct, Float arguments are
+    typecasted to ints before addition is performed.
+    Return the integer addition of a and b by default return 98.
     TypeError: If either of a or b is a non-integer and non-float.
     """
 
     if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
-    elif type(b) is not int and type(b) is not float:
+    if type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
 
     a = int(a)
