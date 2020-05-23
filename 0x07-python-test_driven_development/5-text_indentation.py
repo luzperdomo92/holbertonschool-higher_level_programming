f52edfa7d3a2fb1a4f80dc92ee59c035cd450f6e
Luz Perdomo <lilyperdomo92@gmail.com>
2020-05-22 19:21:46 -0500

    fixed logial syntax, added booleand logical

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -3,11 +3,23 @@ def text_indentation(text):
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
+    should_print = True
+
     for iter, character in enumerate(text):
-        if (text[iter - 1] == "." or text[iter - 1] == "?" or
-                text[iter - 1] == ":") and character == " ":
+
+        if character == " ":
+            if iter == 0 or not should_print:
+                should_print = False
+                continue
+            else:
+                print(character, end="")
+                continue
+
+        if character == "." or character == "?" or character == ":":
+            print(character)
+            print("")
+            should_print = False
             continue
 
+        should_print = True
         print(character, end="")
-        if character == "." or character == "?" or character == ":":
-            print("\n")
