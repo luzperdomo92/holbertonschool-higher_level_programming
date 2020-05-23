99d5c8e718398e517bc4a1e3eb39bd522e69bf9c
Luz Perdomo <lilyperdomo92@gmail.com>
2020-05-23 02:22:52 -0500

    changes to add_integer

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -11,9 +11,9 @@ def add_integer(a, b=98):
     TypeError: If either of a or b is a non-integer and non-float.
     """
 
-    if type(a) is not int and type(a) is not float::
+    if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
-    if type(a) is not int and type(a) is not float::
+    if type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
 
     a = int(a)
