d7c02dc8ca7e6b55a2280eec54329a1165da0208
Luz Perdomo <lilyperdomo92@gmail.com>
2020-06-02 19:19:25 -0500

    fixed pycodestyle

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def lookup(obj):
-    """ 
-    methot that returns the 
-    list of available attributes 
+    """
+    methot that returns the
+    list of available attributes
     and methods of an object:
     """
     return(list(dir(obj)))
