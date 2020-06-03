3d35072ab2435fa488e7de677c3a0c6a1e11007f
Luz Perdomo <lilyperdomo92@gmail.com>
2020-06-03 16:56:40 -0500

    fixed name of param

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,5 +7,5 @@ def read_file(filename=""):
     Function to read a file
     and print it in the STOUT
     """
-    with open("my_file_0.txt", "r") as fl:
+    with open(filename, "r") as fl:
         print(fl.read())
