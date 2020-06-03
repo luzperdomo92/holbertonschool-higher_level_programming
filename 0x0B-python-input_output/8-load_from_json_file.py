1c82cb48a5be12d01f1a01e271a9316f6e562f32
Luz Perdomo <lilyperdomo92@gmail.com>
2020-06-03 17:51:32 -0500

    fixed load acces

--- 0x0B-python-input_output/8-load_from_json_file.py
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -8,6 +8,6 @@ def load_from_json_file(filename):
     Function function that creates
     an Object from a “JSON file”
     """
-    with open(filename, "x") as fl:
-        new_obj = json.loads(fl)
+    with open(filename) as fl:
+        new_obj = json.load(fl)
         return (new_obj)
