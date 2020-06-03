56121e7d5d6184efbe5449d3a1a47e7d33f1370d
Luz Perdomo <lilyperdomo92@gmail.com>
2020-06-03 18:56:11 -0500

    adde task 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Defining new class Student"""
+
+
+class Student():
+    """Class Student"""
+
+    def __init__(self, first_name, last_name, age):
+        """constructor to define parameters"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """ Method that return dictionary representation"""
+        return self.__dict__
