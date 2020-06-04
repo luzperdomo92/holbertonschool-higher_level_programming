aa0c5ff8b9faf52a3b6656376383d3423144fbda
Luz Perdomo <lilyperdomo92@gmail.com>
2020-06-03 20:31:23 -0500

    added task 13

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Defining new class Student"""
+
+
+class Student():
+    """Class Student"""
+
+    def __init__(self, first_name, last_name, age):
+        """constructor to define parameters"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ Method that return dictionary representation"""
+
+        new_dict = {}
+        if attrs is None:
+            return (self.__dict__)
+
+        for iter in attrs:
+            if hasattr(self, iter):
+                new_dict[iter] = getattr(self, iter)
+        return (new_dict)
+
+    def reload_from_json(self, json):
+        """ Method tat replace all atributes"""
+
+        for key, value in json.items():
+            if hasattr(self, key):
+                setattr(self, key, value)
