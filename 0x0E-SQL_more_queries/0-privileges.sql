c0622e27a823aabb265af893008f3a44052fc50f
Luz Perdomo <lilyperdomo92@gmail.com>
2020-07-14 14:32:31 -0500

    update file that list all provileges of user

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
---  lists all privileges of the MySQL users on your server (in localhost).
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+-- lists all privileges of the MySQL users on your server (in localhost).
+
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
