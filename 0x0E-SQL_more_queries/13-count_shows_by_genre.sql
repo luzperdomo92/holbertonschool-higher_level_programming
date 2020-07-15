20c6c349046afdcbba92f7a856efad8807a3969c
Luz Perdomo <lilyperdomo92@gmail.com>
2020-07-14 19:23:23 -0500

    fixed logical syntax

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -3,6 +3,6 @@
 
 SELECT tv_genres.name AS genre, COUNT(show_id) AS number_of_shows
 FROM tv_genres 
-JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-GROUP BY genre
+JOIN tv_show_genres ON tv_genres.id = tv_genres.id
+GROUP BY tv_show_genres.genre_id
 ORDER BY number_of_shows DESC;
