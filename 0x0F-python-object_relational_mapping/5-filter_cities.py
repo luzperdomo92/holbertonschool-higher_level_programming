e24b11e336aac0565f5a527a55af040c7d88fea3
Luz Perdomo <lilyperdomo92@gmail.com>
2020-08-08 13:33:47 -0500

    added task 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     cursor.execute("""SELECT cities.name FROM cities
                    INNER JOIN states ON cities.state_id = states.id
                    WHERE states.name = %s ORDER BY cities.id""",
-                (argv[4], ))
+                   (argv[4], ))
     rows_select = cursor.fetchall()
     print(", ".join([row[0] for row in rows_select]))
     cursor.close()
