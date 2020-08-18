c719d6a01346075e8a6f4ca3666cf06fa21614b7
Luz Perdomo <lilyperdomo92@gmail.com>
2020-08-18 16:34:30 -0500

    added task 3

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #  takes in a URL and displays all HTTP methods the server will accept.
-# without the word "allow: "
-curl -sI "$1" | grep "Allow" | cut -d " " -f2-
+# without the word allow:
+curl -sI "$1" | grep "Allow" | cut -d " " -f 2-
