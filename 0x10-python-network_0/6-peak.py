99158885b6469412d5834a0465c663b6f2616a12
Luz Perdomo <lilyperdomo92@gmail.com>
2020-08-18 21:16:09 -0500

    fixed peak function

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,9 +4,14 @@
 def find_peak(list_of_integers):
     """ function to finds a peak in a list of unsorted integers."""
     size = len(list_of_integers)
-    if size < 3:
+    if size < 1:
         return None
-    
+    elif size == 1 or (size == 2 and
+                       list_of_integers[0] >= list_of_integers[0]):
+        return list_of_integers[0]
+    elif list_of_integers[size - 1] >= list_of_integers[size - 2] :
+        return list_of_integers[size - 1]
+
     for i in range(1, size - 1):
         if (list_of_integers[i]>= list_of_integers[i - 1] and
             list_of_integers[i]>= list_of_integers[i + 1]):
