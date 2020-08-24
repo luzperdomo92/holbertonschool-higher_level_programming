528e39b91b51dbf650c03ecbcad71c2c3f626f8b
Luz Perdomo <lilyperdomo92@gmail.com>
2020-08-24 17:19:30 -0500

    added task 6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+    script that takes in a URL, sends a request to the URL
+    and displays the value of the variable X-Request-Id in the response header
+ """
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = {'email': sys.argv[2]}
+    post_data = requests.post(url, data= email)
+    print("{}".format(post_data.text))
