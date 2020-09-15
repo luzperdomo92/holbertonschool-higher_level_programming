989531c8cd8a3891a679752110d4a1a3479f31e8
Luz Perdomo <lilyperdomo92@gmail.com>
2020-09-15 17:29:54 -0500

    added task 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const phrases = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const phrasesLen = phrases.length;
+let iter;
+
+for (iter = 0; iter < phrasesLen; iter++) {
+  console.log(phrases[iter]);
+}
