8c8e143145aa38ffcd32b38212c65a5e40bce065
Luz Perdomo <lilyperdomo92@gmail.com>
2020-09-15 15:36:22 -0500

    fixed plural typo

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 if (process.argv.length === 3) {
-  console.log('Arguments found');
+  console.log('Argument found');
 } else if (process.argv.length > 3) {
   console.log('Arguments found');
 } else {
